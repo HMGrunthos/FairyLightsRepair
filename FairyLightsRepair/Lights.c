82d27ffe5315bb9c4039a94e35a2aded0d5459dc
Sam Hole <sam_hole@hotmail.com>
2019-10-31 04:58:13 +0000

    Tidied up the mode implementation. Added inline specs to the Random header file.

--- FairyLightsRepair/Lights.c
+++ FairyLightsRepair/Lights.c
@@ -12,14 +12,12 @@
 
 #include "Random.h"
 
-#define TIMEROUTPUT
-
 enum DisaplyMode {
 	High,
-	Medium,
+	Medium1,
+	Medium2,
 	Low,
 	Breathe,
-	Pulse,
 	Flicker,
 	Flash,
 	Off
@@ -33,7 +31,6 @@ static void prepareForOffMode(void);
 static void prepareForOnModes(void);
 static void waitForButtonRelease(void);
 
-volatile enum DisaplyMode currentMode;
 volatile int8_t pulseCommand = 0;
 
 int main(void)
@@ -42,126 +39,78 @@ int main(void)
 	
 	random16InitFromSeed(0xBEEF);
 
+	prepareForOffMode(); // The system immediately goes into low power mode so we prepare for this
+
 	uint8_t modeState;
 	uint8_t butonState = 0xFE;
-	currentMode = Off;
+	enum DisaplyMode currentMode = Off;
 	while(1) {
 		// Definition of input control
-		butonState = (butonState << 1) | (PINB & (1 << PINB1)) | (0xE0); // De-bounce the input with some don't cares so that the buttons are responsive
+		butonState = (butonState << 1) | (PINB & (1 << PINB1)) | (0xE0); // De-bounce the input with some don't cares (so that the button remains responsive)
 		if(butonState == 0xF0) { // Button pressed
 			currentMode++; // Next mode on button pressed
-			modeState = 0;
-			cli();
-				pulseCommand = 0; // Reset the pulse command on entry to a new mode
-				OCR0A = 0;
-			sei();
 		}
 		
 		// Definition of output behavior
 		switch(currentMode & MODEMASK) {
 			case High:
 				cli();
-					#ifdef TIMEROUTPUT
-						OCR0A = 0xFF;
-					#endif
+					OCR0A = 0xFF;
 				sei();
-				#ifndef TIMEROUTPUT
-					PORTB |= (1 << PORTB0); // Lights on
-				#endif
 				break;
-			case Medium:
+			case Medium1:
 				cli();
-					#ifdef TIMEROUTPUT
-						OCR0A = 0x80;
-					#endif
+					OCR0A = 0x90;
 				sei();
-				#ifndef TIMEROUTPUT
-					PINB |= (1 << PINB0); // Lights toggle
-				#endif
+				break;
+			case Medium2:
+				cli();
+					OCR0A = 0x50;
+				sei();
 				break;
 			case Low:
 				cli();
-					#ifdef TIMEROUTPUT
-						OCR0A= 0x40;
-					#endif
+					OCR0A = 0x20;
 				sei();
-				#ifndef TIMEROUTPUT
-					modeState++;
-					if((modeState & 0x1) == 0) {
-						PINB |= (1 << PINB0); // Lights toggle slower
-					}
-				#endif
 				break;
 			case Breathe:
 				cli();
-					#ifdef TIMEROUTPUT
-						if(OCR0A >= (256-BREATHEPERIOD)) {
-							pulseCommand = -BREATHEPERIOD;
-						} else if(OCR0A < 32) {
-							pulseCommand = BREATHEPERIOD;
-						}
-					#endif
+					if(OCR0A >= (256-BREATHEPERIOD)) {
+						pulseCommand = -BREATHEPERIOD;
+					} else if(OCR0A <= 0x20) {
+						pulseCommand = BREATHEPERIOD;
+					}
 				sei();
-				#ifndef TIMEROUTPUT
-					PORTB |= (1 << PORTB0); // Lights on
-				#endif
-				break;
-			case Pulse:
-				cli();
-					#ifdef TIMEROUTPUT
-						pulseCommand = 1;
-					#endif
-				sei();
-				#ifndef TIMEROUTPUT
-					PINB |= (1 << PINB0); // Lights toggle
-				#endif
 				break;
 			case Flicker:
-				#ifdef TIMEROUTPUT
-					modeState++;
-					if((modeState & 0x3) == 0) {
-						if((random16() + (1<<15)) < (1<<15)) {
-							cli();
-								OCR0A = 0xFF;
-							sei();
-						} else {
-							cli();
-								OCR0A = 0x00;
-							sei();
-						}
+				pulseCommand = 0;
+				modeState++;
+				if((modeState & 0x3) == 0) {
+					if((random16() + (1<<15)) < (1<<15)) {
+						cli();
+							OCR0A = 0xFF;
+						sei();
+					} else {
+						cli();
+							OCR0A = 0x02;
+						sei();
 					}
-				#endif
-				#ifndef TIMEROUTPUT
-					PORTB |= (1 << PORTB0); // Lights on
-				#endif
+				}
 				break;
 			case Flash:
 				cli();
-					#ifdef TIMEROUTPUT
-						modeState++;
-						if((modeState & 0x7) == 0) {
-							if(OCR0A == 0xFF) {
-								OCR0A = 0x00;	
-							} else {
-								OCR0A = 0xFF;
-							}
-						}
-					#endif
+					modeState++;
+					if(modeState & 0x8) {
+						OCR0A = 0x02;
+					} else {
+						OCR0A = 0xFF;
+					}
 				sei();
-				#ifndef TIMEROUTPUT
-					PINB |= (1 << PINB0); // Lights toggle
-				#endif
 				break;
 			case Off:
 				cli();
-					#ifdef TIMEROUTPUT
-						pulseCommand = 0;
-						OCR0A = 0x10;
-					#endif
+					OCR0A = 0x10;
 				sei();
-				#ifndef TIMEROUTPUT
-					PORTB &= ~(1 << PORTB0); // Lights off
-				#endif
 				waitForButtonRelease();
 				prepareForOffMode();
 				break;
@@ -178,11 +127,7 @@ int main(void)
 		sei();
 		if((currentMode & MODEMASK) == Off) { // On waking from the off state go into the on state
 			prepareForOnModes();
-			#ifdef TIMEROUTPUT
-				OCR0A = 0xFF;
-			#else
-				PORTB |= (1 << PORTB0); // Lights on
-			#endif
+			OCR0A = 0xFF;
 			waitForButtonRelease(); // Wait for the button to be released
 			currentMode = High;
 			butonState = 0xFE;
@@ -198,12 +143,8 @@ static void initHW(void)
 	DDRB |= (1 << DDB0); // Lights pin set as an output
 	PORTB |= (1 << PORTB1) | (1 << PORTB2) | (1 << PORTB3) | (1 << PORTB4) | (1 << PORTB5); // Enable the pullups on all the digital inputs
 
-	#ifdef TIMEROUTPUT
-		TCCR0A = (1<<COM0A1) | (0<<COM0A0) | (1 << WGM00); // PWM(Phase Correct)
-		TCCR0B = (1 << CS00); // Divide input clock by 1
-	#endif
-
-	prepareForOffMode(); // The system starts off turned off
+	TCCR0A = (1<<COM0A1) | (0<<COM0A0) | (1 << WGM00); // PWM(Phase Correct)
+	TCCR0B = (1 << CS00); // Divide input clock by 1
 
 	sei();
 }
