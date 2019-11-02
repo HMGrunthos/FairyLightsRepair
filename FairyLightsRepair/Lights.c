13d9871b85ba4c44fdafeec8b6422e03c0cf861f
Sam Hole <sam_hole@hotmail.com>
2019-11-02 21:07:53 +0000

    Implemented timer based wakeup (which should be better than the watchdog timer)

--- FairyLightsRepair/Lights.c
+++ FairyLightsRepair/Lights.c
@@ -26,10 +26,14 @@ enum DisaplyMode {
 	Off
 };
 
+// #define BREATHINGDIVIDER(x) (3*((x) >> 2))
 #define BREATHINGDIVIDER(x) ((x) >> 1)
 #define BREATH_DXINXSCALE_ASPOWER 3
 #define BREATH_GRADSCALEPWR 2
 static const uint8_t breathTable[33] PROGMEM = {255, 245, 203, 165, 136, 112, 91, 75, 62, 51, 42, 35, 29, 24, 20, 16, 13, 10, 8, 7, 7, 8, 11, 17, 25, 36, 52, 72, 97, 131, 170, 216, 255};
+
+static volatile uint16_t sleepCounter = 0;
+static volatile uint_fast8_t timerWake = 0;
 
 static void initHW(void);
 static void prepareForOffMode(void);
@@ -42,8 +46,6 @@ int main(void)
 	initHW();
 
 	random16InitFromSeed(0xBEEF);
-
-	prepareForOffMode(); // The system immediately goes into low power mode so we prepare for this
 
 	uint16_t modeTimer = 0;
 	uint8_t butonState = 0xFE;
@@ -84,22 +86,40 @@ int main(void)
 				break;
 			case Off:
 				// Get ready to turn off
-				OCR0A = 0x01;
+				OCR0A = 0x01; // Low intensity while we hold the button down
 				waitForButtonRelease();
 				prepareForOffMode();
 				break;
 		}
 		modeTimer++;
 
-		// Go to sleep and wait for the next io update
-		if(currentMode == Off) {
-			sleep_enable();
-			sleep_bod_disable();
-			sleep_cpu();
-			sleep_disable();
-		} else {
-			sleep_mode();
-		}
+		uint_fast8_t wakeUp;
+		do {
+			// Go to sleep and wait for the next IO update
+			if(currentMode == Off) {
+				sleep_enable();
+				sleep_bod_disable();
+				sei();
+				sleep_cpu();
+				sleep_disable();
+				break;
+			} else {
+				cli();
+				if(!timerWake) {
+					sleep_enable();
+					sei();
+					sleep_cpu();
+					sleep_disable();
+				}
+				sei();
+			}
+			cli();
+				wakeUp = timerWake;
+			sei();
+		} while(!wakeUp);
+		cli();
+			timerWake = 0;
+		sei();
 
 		// On waking from off mode
 		if(currentMode == Off) { // On waking from the off state go into the breathing state
@@ -121,8 +141,11 @@ static void initHW(void)
 	DDRB |= (1 << DDB0); // Lights pin set as an output
 	PORTB |= (1 << PORTB1) | (1 << PORTB2) | (1 << PORTB3) | (1 << PORTB4) | (1 << PORTB5); // Enable the pullups on all the digital inputs
 
-	TCCR0A = (1<<COM0A1) | (0<<COM0A0) | (1 << WGM00); // PWM(Phase Correct)
-	TCCR0B = (1 << CS00); // Divide input clock by 1
+	TCCR0A = (1 << COM0A1) | (0 << COM0A0) | (1 << WGM00); // PWM(Phase Correct)
+	TCCR0B = (1 << CS00); // Divide input clock by 1 to give a clock at 600000 and a TOV0 at 1.176470588kHz
+	TIMSK0 = (1 << TOIE0); // Enable the overflow interrupt
+
+	PRR = (1 << PRADC); // We don't need the ADC so turn it off
 
 	sei();
 }
@@ -132,22 +155,35 @@ static void waitForButtonRelease(void)
 	uint8_t butonStateUp = 0xFC; // Assue the button is pressed to begin with
 	do {
 		butonStateUp = (butonStateUp << 1) | (PINB & (1 << BUTTONPIN)); // De-bounce the input
-		_delay_ms(16);
+
+		// Wait for the next timer tick
+		uint_fast8_t wakeCmd;
+		do {
+			cli();
+				wakeCmd = timerWake;
+			sei();
+		} while(wakeCmd == 0);
+		cli();
+			timerWake = 0;
+		sei();
 	} while(butonStateUp != 0x7E); // This loop waits for the button to go high (i.e. be released.)
 }
 
 static void prepareForOffMode(void)
 {
 	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-	WDTCR &= ~(1 << WDTIE); // Watchdog timer off
 	GIMSK |= (1 << INT0); // Enable external interrupts on INT0
+	TIMSK0 &= ~(1 << TOIE0); // Turn off timer interrupt
+	TCCR0A &= ~(1 << COM0A1) & ~(0 << COM0A0); // Turn off timer controlled PWM pin
+	PORTB &= ~(1 << PORTB0); // Lights off
 }
 
 static void prepareForOnModes(void)
 {
 	set_sleep_mode(SLEEP_MODE_IDLE);
-	WDTCR |= (1 << WDTIE); // Watchdog timer trips every 16ms-ish so we keep monitoring the button and updating the timer
 	GIMSK &= ~(1 << INT0); // Disable external interrupts on INT0
+	TIMSK0 |= (1 << TOIE0); // Turn back on timer interrupt
+	TCCR0A |= (1 << COM0A1) | (0 << COM0A0); // Turn on the PWM pin
 }
 
 static uint8_t getBreathIntensity(const uint8_t findMe)
@@ -176,8 +212,12 @@ static uint8_t getBreathIntensity(const uint8_t findMe)
 	return val;
 }
 
-ISR(WDT_vect)
+ISR(TIM0_OVF_vect)
 {
+	sleepCounter++;
+	if((sleepCounter & 0x0000000F) == 0) {
+		timerWake = 1; // Wake at 73.5Hz when running with clock divider set to 1
+	}
 }
 
 ISR(INT0_vect)
